ed879530f8f46f1ba7ef65cde2d69436a6a6c103
VastCosmic <137017677@qq.com>
2023-06-23 20:45:11 +0800

    delete webapi

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,4 @@
-﻿using Grpc.Net.Client;
-using Quartz.Impl;
-using Quartz;
-using SwitchService;
-using Client.QuartzJob;
-
+﻿using Client.QuartzJob;
 
 /*****************************************************************
  * 以下为 job 测试代码
